c39c1bc9bf28654fb15b75f98e405ae83720d807
Rhythm-Gubrani <131393410+Rhythm-Gubrani@users.noreply.github.com>
2023-08-06 23:38:39 +0530

    Update Q2.cpp

--- lab_1/Q2.cpp
+++ lab_1/Q2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout<<"This code is developed by Rajat Khandelwal 21103167 "<<endl;
+    cout<<"My name is RHYTHM and my enroolment no. is 21103168 "<<endl;
     cout<<"This is my first assignment of week 1"<<endl;
     return 0;
 }

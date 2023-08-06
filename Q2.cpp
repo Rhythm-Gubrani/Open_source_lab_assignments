89fdd283a604f1a1444c90e098558930b97cd8c1
Rhythm-Gubrani <131393410+Rhythm-Gubrani@users.noreply.github.com>
2023-08-06 18:38:16 +0000

    commit

--- Q2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"This code is developed by Rajat Khandelwal 21103167 "<<endl;
-    cout<<"This is my first assignment of week 1"<<endl;
-    return 0;
-}

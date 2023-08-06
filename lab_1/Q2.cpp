fa17daad49d8ae57f63a3aeccd69aeed0aa4d357
Rhythm-Gubrani <131393410+Rhythm-Gubrani@users.noreply.github.com>
2023-08-06 23:37:14 +0530

    Add files via upload

---
+++ lab_1/Q2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"This code is developed by Rajat Khandelwal 21103167 "<<endl;
+    cout<<"This is my first assignment of week 1"<<endl;
+    return 0;
+}
